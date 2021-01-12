03f935d4ffac17b204f1d51aba4a7b7207e2264e
Huỳnh Minh Khôi <61672720+liam1908@users.noreply.github.com>
2021-01-12 12:57:11 +0700

    Add files via upload

---
+++ Data.cs
@@ -0,0 +1,80 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Data.cs" company="Beckhoff Automation GmbH & Co. KG">
+//     Copyright (c) Beckhoff Automation GmbH & Co. KG. All Rights Reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace SE_Test
+{
+    // Contains runtime data. This class should be thread-safe.
+    public class Data
+    {
+        private int maxRandom = 1000;
+        private object maxRandomLock = new object();
+
+        private bool boolTestVar = false;
+        private object boolTestVarLock = new object();
+
+        private bool exportExcel = false;
+        private object exportExcelLock = new object();
+
+        
+
+        public int MaxRandom
+        {
+            get
+            {
+                lock (this.maxRandomLock)
+                {
+                    return this.maxRandom;
+                }
+            }
+
+            set
+            {
+                lock (this.maxRandomLock)
+                {
+                    this.maxRandom = value;
+                }
+            }
+        }
+
+        public bool BoolTestVar
+        {
+            get
+            {
+                lock (this.boolTestVarLock)
+                {
+                    return this.boolTestVar;
+                }
+            }
+
+            set
+            {
+                lock (this.boolTestVarLock)
+                {
+                    this.boolTestVar = value;
+                }
+            }
+        }
+
+        public bool ExportExcel
+        {
+            get
+            {
+                lock (this.exportExcelLock)
+                {
+                    return this.exportExcel;
+                }
+            }
+
+            set
+            {
+                lock (this.exportExcelLock)
+                {
+                    this.exportExcel = value;
+                }
+            }
+        }
+    }
+}
---
+++ ExtensionErrorValue.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ExtensionErrorValue.cs" company="Beckhoff Automation GmbH & Co. KG">
+//     Copyright (c) Beckhoff Automation GmbH & Co. KG. All Rights Reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace SE_Test
+{
+    // Contains extension specific error values.
+    public static class ExtensionErrorValue
+    {
+        public static readonly uint HmiExtSuccess = 0x0;
+        public static readonly uint HmiExtFail = 0x1;
+    }
+}
